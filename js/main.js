7551af821a6064d84efc43e68127a7ea584b6cfb
sergiopardo <xerchpardo@gmail.com>
2017-04-04 20:08:14 -0500

    preferences list & reload btn

--- js/main.js
+++ js/main.js
@@ -9,37 +9,48 @@ var ObjectLike = {
 
 var blackCollection = [{
 	"name":"black1",
-	"imgSrc":"img/list-covers/black/black-1.png"
+	"imgSrc":"img/list-covers/black/black-1.png",
+	"smallCover":"img/list-covers/black/small/lib1.png"
 },{
 	"name":"black2",
-	"imgSrc":"img/list-covers/black/black-2.png"
+	"imgSrc":"img/list-covers/black/black-2.png",
+	"smallCover":"img/list-covers/black/small/lib2.png"
 },{
 	"name":"black3",
-	"imgSrc":"img/list-covers/black/black-3.png"
+	"imgSrc":"img/list-covers/black/black-3.png",
+	"smallCover":"img/list-covers/black/small/lib3.png"
 },{
 	"name":"black4",
-	"imgSrc":"img/list-covers/black/black-4.png"
+	"imgSrc":"img/list-covers/black/black-4.png",
+	"smallCover":"img/list-covers/black/small/lib4.png"
 },{
 	"name":"black5",
-	"imgSrc":"img/list-covers/black/black-5.png"
+	"imgSrc":"img/list-covers/black/black-5.png",
+	"smallCover":"img/list-covers/black/small/lib5.png"
 },{
 	"name":"black6",
-	"imgSrc":"img/list-covers/black/black-6.png"
+	"imgSrc":"img/list-covers/black/black-6.png",
+	"smallCover":"img/list-covers/black/small/lib6.png"
 },{
 	"name":"black7",
-	"imgSrc":"img/list-covers/black/black-7.png"
+	"imgSrc":"img/list-covers/black/black-7.png",
+	"smallCover":"img/list-covers/black/small/lib7.png"
 },{
 	"name":"black8",
-	"imgSrc":"img/list-covers/black/black-8.png"
+	"imgSrc":"img/list-covers/black/black-8.png",
+	"smallCover":"img/list-covers/black/small/lib8.png"
 },{
 	"name":"black9",
-	"imgSrc":"img/list-covers/black/black-9.png"
+	"imgSrc":"img/list-covers/black/black-9.png",
+	"smallCover":"img/list-covers/black/small/lib9.png"
 },{
 	"name":"black10",
-	"imgSrc":"img/list-covers/black/black-10.png"
+	"imgSrc":"img/list-covers/black/black-10.png",
+	"smallCover":"img/list-covers/black/small/lib10.png"
 },{
 	"name":"black11",
-	"imgSrc":"img/list-covers/black/black-1.png"
+	"imgSrc":"img/list-covers/black/black-1.png",
+	"smallCover":"img/list-covers/black/small/lib11.png"
 }]
 
 function fillObject(category,item){
@@ -84,11 +95,16 @@ function clickPanel(selected){
 }
 
 function fillCollectionSwiper(collection){
-	var selectedCollection = collection;
 	console.log(collection)
+	var selectedCollection = collection;
+
+	if (collection == undefined){
+		var selectedCollection = blackCollection
+	}
+	console.log(selectedCollection)
 	$("#swipe-wrapper ul").empty()
 	for(i=0; i<selectedCollection.length; i++){
-		var swipePanel = 	"<li class='pane pane"+(i+1)+"' data-coleccion='black' data-nombre='Black "+(i+1)+"' data-imagen='"+selectedCollection[i].imgSrc+"'>"+
+		var swipePanel = 	"<li class='pane pane"+(i+1)+"' data-coleccion='black' data-nombre='Black "+(i+1)+"' data-imagen='"+selectedCollection[i].smallCover+"'>"+
 						"<div class='img-wrapper'> <img src='"+selectedCollection[i].imgSrc+"' alt=''> </div>"+
 						"<p class='slide-name'>Black</p>"+
 						"<div class='like'></div>"+
@@ -143,12 +159,14 @@ function fillPreferencesList(listType){
 	}
 	$(".list-wrapper").empty();
 	for(i=0; i<selectedList.length; i++){
-        var selectedCard = 	"<div class='col-xs-12 col-sm-4 list-card'>" +
+        var selectedCard = 	"<div class='col-xs-12 col-sm-4'>" +
+        					"<div class='list-card'>"+
 				            "<img src='"+selectedList[i].imagen+"' alt=''>" +
 				            "<p class='card-name'>"+selectedList[i].nombre+"</p>" +
 				            "<div class='like-count'>" +
 				            "<span class='counter'>"+Math.floor((Math.random() * 500) + 100)+"</span>" +
 				            "<div class='like-btn'></div>" +
+				            "</div>" +
 				            "</div>" +
 				            "</div>";
 		$(".list-wrapper").append(selectedCard)
