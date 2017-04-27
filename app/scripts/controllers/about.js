d03141de82df1815285f83d6de88a0707dc43cf3
Jorge Vargas <Jorge Vargas>
2017-04-27 14:15:01 -0500

    Creacion de objeto para llenado de datos de usuario, y geolocalizacion

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -14,8 +14,25 @@ angular.module('scribeApp')
       'AngularJS',
       'Karma'
     ];
+
+    $scope.dataUser = [];
+
+    $scope.edad;
+    $scope.mapa_lat;
+    $scope.mapa_long;
+
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position){
+        $scope.$apply(function(){
+          $scope.mapa_lat = position.coords.latitude;
+          $scope.mapa_long = position.coords.longitude;
+
+        });
+      });
+    }
     
     $(".genero img").click(function () {
+      $scope.genero = $(this).data("genero");
       $(".genero img").css("transform", "scale(1)");
       //$(this).attr("src","img/dislike-btn.svg")
       $(this).css({
@@ -23,5 +40,15 @@ angular.module('scribeApp')
       });
     });
 
-    
+
+    $scope.viewData =  function(){
+        $scope.dataUser.push({
+          genero:$scope.genero,
+          edad:$scope.edad,
+          mapa_lat:$scope.mapa_lat,
+          mapa_long:$scope.mapa_long
+      });
+    }
+
+
   });
